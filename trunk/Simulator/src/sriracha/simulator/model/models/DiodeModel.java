93faf7bcb04a131be374a0995adb8f41b57d3fe8
yiqing.xiao <xiaoyiqing90@gmail.com>
2013-10-29 22:15:41 -0400

    Created a diode model class

--- trunk/Simulator/src/sriracha/simulator/model/models/DiodeModel.java
+++ trunk/Simulator/src/sriracha/simulator/model/models/DiodeModel.java
@@ -7,5 +7,23 @@ package sriracha.simulator.model.models;
  * Time: 9:27 PM
  * To change this template use File | Settings | File Templates.
  */
-public class DiodeModel {
+public class DiodeModel extends CircuitElementModel{
+
+    private double is;
+
+    public DiodeModel(char key, String name, String line) {
+        super(key, name);
+
+        //parse the ".model" line's characteristics between parentheses into separate Strings
+        //example: .model mName D (IS=0 RS=0 ...)
+        String[] parameters = line.substring(line.indexOf("(")+1, line.indexOf(")")).split("\\s+");
+
+        for(String s: parameters){
+            String characteristicName = s.substring(0,s.indexOf("="));
+            characteristicName.toLowerCase();
+            if(characteristicName.equals("is")){
+                is = Double.parseDouble(s.substring(s.indexOf("=")+1, s.length()-1));
+            }
+        }
+    }
 }
